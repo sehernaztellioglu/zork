151bca0a8088e48428e89d28c0b74a4896adf1fc
sehernaztellioglu <sehernaztellioglu@gmail.com>
2026-09-09 01:20:26 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,26 @@ import java.util.Scanner;
 
 class main {
 
+    public static boolean kelimeVarMi (String Input, String expected){
+
+
+        String[] kelimeler = Input.split("\\s+");
+        for(String eleman : kelimeler){
+            if(eleman.equals(expected)){
+
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean envanterGoster(String Input){
+        if(Input.equals("show inventory")){
+            System.out.println("showing inventory");
+            return true;
+        }
+        return false;
+    }
+
     public void main(String[] arg) {
 
         System.out.println("you're in a forest in front of a cave there are monsters in the forest.");
@@ -17,38 +37,25 @@ class main {
         int sayac = 0;
 
 
-        LinkedList<String> firstInput = new LinkedList<String>();
 
+        LinkedList<String> envanter = new LinkedList<String>();
 
-        String Input = scanner.nextLine().toLowerCase();
-        firstInput.add(Input);
-        String[] kelimeler = Input.split("\\s+");
+        
 
         while (sayac == 0) {
+            String Input = scanner.nextLine().toLowerCase();
+            if(kelimeVarMi(Input,"north")){
+                System.out.println("you moved further into the cave still can't see anything");
+            }else if(kelimeVarMi(Input,"east")){
+                System.out.println("you moved further into the cave still can't see anything");
+            }else if (kelimeVarMi(Input,"west")){
+                System.out.println("you moved further into the cave still can't see anything");
+            }else if (kelimeVarMi(Input,"south")){
+                System.out.println("you moved further into the cave still can't see anything");
+            }else if(envanterGoster(Input)){
 
-            for (String eleman : kelimeler) {
-
-                if (eleman.equals("north")) {
-                    System.out.println("you went inside the cave but you can't see anything");
-                    sayac++;
-
-                }
-                if (eleman.equals("east")) {
-                    System.out.println("endless forest you decided the cave is your only option");
-                    sayac++;
-
-                }
-                if (eleman.equals("west")) {
-                    System.out.println("endless forest you decided the cave is your only option");
-                    sayac++;
-
-                }
-                if (eleman.equals("south")) {
-                    System.out.println("you saw a troll and decided to go inside the cave to hide");
-                    sayac++;
-
-
-                }
+            }else{
+                System.out.println("i don't understand");
             }
         }
     }
